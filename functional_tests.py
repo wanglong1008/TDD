d2835ff4f0d3b948f421692ba5e28bdecfad84c6
wanglong1008 <925367077@qq.com>
2021-04-16 13:59:06 +0800

    Refactor home page view to use a template

--- functional_tests.py
+++ functional_tests.py
@@ -4,21 +4,21 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 
-class NewVisitorTest(unittest.TestCase):#(1)
+class NewVisitorTest(unittest.TestCase):	#(1)
 
-	def setUp(self): #(3)
+	def setUp(self): 		#(3)
 		self.browser = webdriver.Firefox()
 
-	def tearDown(self): # (3)
+	def tearDown(self): 	# (3)
 		self.browser.quit()
 
-	def test_can_start_a_list_and_retrieve_it_later(self): # (2)
+	def test_can_start_a_list_and_retrieve_it_later(self): 		# (2)
 		# Edith has heard about a cool new online to-do app. She goes
 		# to check out its homepage
 		self.browser.get('http://localhost:8000')
 
 		# She notices the page title and header mention to-do lists
-		self.assertIn('To-Do', self.browser.title)# (4)
+		self.assertIn('To-Do', self.browser.title)		# (4)
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
 
@@ -38,8 +38,9 @@ class NewVisitorTest(unittest.TestCase):#(1)
 		self.assertTrue(
 			any(row.text == '1: Buy peacock feathers' for row in rows)
 			)
-		self.fail('Finish the test!') # (5)
-		
-if __name__== '__main__' : # (6)
-	unittest.main(warnings = 'ignore')# (7)
+		self.fail('Finish the test!')   # (5)
+
+
+if __name__ == '__main__': 			# (6)
+	unittest.main(warnings = 'ignore')		# (7)
 
--- lists/tests.py
+++ lists/tests.py
@@ -1,20 +1,28 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolve_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    # def test_root_url_resolve_to_home_page_view(self):
+    #     found = resolve('/')
+    #     self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))  # 4
+        response = self.client.get('/')
+        #
+        # html = response.content.decode('utf8')
+        # self.assertTrue(html.startswith('<html>'))
+        # self.assertIn('<title>To-Do lists</title>', html)
+        # self.assertTrue(html.endswith('</html>'))  # 4
+
+        self.assertTemplateUsed(response, 'home.html')
+        # request = HttpRequest()
+        # response = home_page(request)
+        # html = response.content.decode('utf8')
+        # expected_html = render_to_string('home.html')
+        # self.assertEqual(html, expected_html)
 # Create your tests here.
