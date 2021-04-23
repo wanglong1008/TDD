9ef95da4dfc3f55d51fe945b5f8c910accfad94f
wanglong1008 <925367077@qq.com>
2021-04-23 14:04:08 +0800

    unit5 fore test

--- lists/tests.py
+++ lists/tests.py
@@ -5,6 +5,7 @@ from django.template.loader import render_to_string
 
 from lists.views import home_page
 
+
 class HomePageTest(TestCase):
 
     # def test_root_url_resolve_to_home_page_view(self):
@@ -25,4 +26,9 @@ class HomePageTest(TestCase):
         # html = response.content.decode('utf8')
         # expected_html = render_to_string('home.html')
         # self.assertEqual(html, expected_html)
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': "A new list item"})
+        self.assertIn("A new list item", response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
 # Create your tests here.
