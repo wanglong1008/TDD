d6f6464462b186cc2a49682a93a1fa96a3357a01
wanglong1008 <925367077@qq.com>
2021-04-23 14:34:37 +0800

    unit5 mid commit

--- functional_tests.py
+++ functional_tests.py
@@ -27,20 +27,33 @@ class NewVisitorTest(unittest.TestCase):	#(1)
 		# 	inputbox.get_attribute('placeholder'),
 		# 	'Enter a to-do item'
 		# 	)
-		inputbox.send_keys('Buy peacock feathers')
+		#inputbox.send_keys('1: Buy peacock feathers')
+		# inputbox.send_keys(Keys.ENTER)
+		# time.sleep(1)
+		# self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
 		# self.assertTrue(
 		# 	any(row.text == '1: Buy peacock feathers' for row in rows),
 		# 	f"New to-do item did not appear in table.Contents were:\n{table.text}"
 		# 	)
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+		# table = self.browser.find_element_by_id('id_list_table')
+		# rows = table.find_elements_by_tag_name('tr')
+		# self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		# self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 		self.fail('Finish the test!')   # (5)
+
+	def check_for_row_in_list_table(self,row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
 
 
 if __name__ == '__main__': 			# (6)
