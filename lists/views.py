61a839bc594e1c508a222d2bfb595fa535f29f6a
wanglong1008 <925367077@qq.com>
2021-05-21 13:59:37 +0800

    unit 7 page 51 commit

--- lists/views.py
+++ lists/views.py
@@ -6,9 +6,6 @@ from lists.models import Item
 def home_page(request):
     # if request.method == 'POST':
     #     return HttpResponse(request.POST['item_text'])
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 
@@ -17,5 +14,8 @@ def view_list(request):
     return render(request, 'list.html', {'items': items})
 
 
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
 # Create your views here.
 
