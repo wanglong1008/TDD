c8db2bcd45e9588f2dc4dff6f9795edd0852643f
wanglong1008 <925367077@qq.com>
2021-06-05 16:34:44 +0800

    add gunicorn to virtualenv requirements.txt

--- superlists/asgi.py
+++ superlists/asgi.py
@@ -14,3 +14,21 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'superlists.settings')
 
 application = get_asgi_application()
+
+# unix:/tmp/39.106.68.230.socket superlists.wsgi:application
+#
+# unix:/root/sites/120.53.108.140.socket
+
+# [Unit]
+# Description=Gunicorn server for 120.53.108.140
+#
+#
+# [Service]
+# Restart=on-failure
+# User=wanglong
+# WorkingDirectory=/root/sites/120.53.108.140/source
+# ExecStart=/root/sites/120.53.108.140/virtualenv/bin/gunicorn --bind unix:/root/sites/120.53.108.140.socket superlists.wsgi:application
+#
+# [Install]
+# WantedBy=multi-user.target
+
